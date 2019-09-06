e49a5ddc04cf1d03d09b05d0f029310d972f39ad
Ut <ub3@student.uwf.edu>
2019-09-06 17:22:19 -0500

    First commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{	
+	int i = 1;
+	cout << endl;
+	cout << "UtB:BuckUt" << endl;
+	while (i <= 100)
+	{	
+		for(int j = 1; j <= 5; j++)
+		{
+            cout << i << ' ';
+            i++;
+        }
+     	cout << endl;
+	}
+	return 0;
+}
